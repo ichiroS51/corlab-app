2fb09ff0df86b7db858d8b3cc52030ce73c59834
oscardv18 <oscarddiazvelasquez@gmail.com>
2022-12-23 00:34:51 -0400

    vuex installed and config createStore

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,8 +1,19 @@
-<script setup>
+<script>
 import HelloWorld from './components/HelloWorld.vue'
+import { mapState } from 'vuex';
+
+export default {
+    components: {
+        HelloWorld,
+    },
+    computed: {
+        ...mapState(['user'])
+    }
+}
 </script>
 
 <template>
+    <pre>{{ user }}</pre>
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
