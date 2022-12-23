a03c57271df3f4493587be1f07b9f772dd8b3189
oscardv18 <oscarddiazvelasquez@gmail.com>
2022-12-23 00:35:15 -0400

    tailwindcss installed successfully

---
+++ vue/src/store/index.js
@@ -0,0 +1,25 @@
+import { createStore } from "vuex"
+
+const store = createStore({
+    state: {
+        user: {
+            data: {name: 'Oscar'},
+            token: null,
+        }
+    },
+    getters: {
+
+    },
+    actions: {
+
+    },
+    mutations: {
+
+    },
+    modules: {
+
+    },
+})
+
+
+export default store;
