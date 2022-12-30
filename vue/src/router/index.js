6c74f696750cc1a44d287132f1880c8523672070
oscardv18 <oscarddiazvelasquez@gmail.com>
2022-12-29 23:39:28 -0400

    routes for login, home page and dashboard completed

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,27 +1,56 @@
-import {createRouter, createWebHistory} from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import Landing from '../views/Landing.vue';
 import Login from '../views/Login.vue';
-import Register from '../views/Register.vue'
+import Logged from '../components/Logged.vue';
+import NotLogged from '../components/NotLogged.vue';
+import NotLog from '../views/NotLog.vue';
+import store from '../store/';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
+            path: '/landing',
+            name: 'Logged',
+            component: Logged,
+            meta: { requiresAuth: true },
+            children: [
+                {
+                    path: '/landing',
+                    name: 'Landing',
+                    component: Landing,
+                },
+            ]
+        },
+        {
             path: '/',
-            name: 'Landing',
-            component: Landing,
-        },
-        {
-            path: '/login',
-            name: 'Login',
-            component: Login,
-        },
-        {
-            path: '/register',
-            name: 'Register',
-            component: Register,
+            name: 'NotLogged',
+            component: NotLogged,
+            meta: { isGuest: true },
+            children: [
+                {
+                    path: '/',
+                    name: 'Notlog',
+                    component: NotLog,
+                },
+                {
+                    path: '/login',
+                    name: 'Login',
+                    component: Login,
+                },
+            ]
         },
     ]
 });
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.state.user.token) {
+        next({ name: "Login" });
+    } else if (store.state.user.token && to.isGuest === 'Login') {
+        next({ name: 'Landing' });
+    } else {
+        next()
+    }
+})
 
 export default router;
