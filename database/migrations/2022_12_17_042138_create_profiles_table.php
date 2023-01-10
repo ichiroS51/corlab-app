e044fd69ed9604b1cd13b98cdddb9ffff9a1f639
oscardv18 <oscarddiazvelasquez@gmail.com>
2023-01-09 21:52:49 -0400

    Migration profiles created and model

---
+++ database/migrations/2022_12_17_042138_create_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            # relationships one to many with clients table
+            $table->unsignedBigInteger('client_id')->nullable();
+            $table->foreign('client_id')
+                ->references('id')
+                ->on('clients')
+                ->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
