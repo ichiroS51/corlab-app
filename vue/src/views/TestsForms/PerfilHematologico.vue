a34f1f1e31599d854d4568d5fe0123bdb4306723
oscardv18 <oscarddiazvelasquez@gmail.com>
2023-01-22 15:18:55 -0400

    testss

--- vue/src/views/TestsForms/PerfilHematologico.vue
+++ vue/src/views/TestsForms/PerfilHematologico.vue
@@ -13,9 +13,10 @@
                         <div class="overflow-hidden shadow sm:rounded-md">
                             <div class="bg-white px-4 py-5 sm:p-6">
                                 <div class="grid grid-cols-6 gap-6">
+
                                     <div class="col-span-6 sm:col-span-3">
                                         <label for="first-name" class="block text-sm font-medium text-gray-700">First
-                                            name</label>
+                                            name <span>(gr.%)</span> <span>(V.N - 1324)</span> </label>
                                         <input type="text" name="first-name" id="first-name" autocomplete="given-name"
                                             class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                     </div>
@@ -27,37 +28,14 @@
                                             class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                     </div>
 
-                                    <div class="col-span-6 sm:col-span-4">
-                                        <label for="email-address" class="block text-sm font-medium text-gray-700">Email
-                                            address</label>
-                                        <input type="text" name="email-address" id="email-address" autocomplete="email"
-                                            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
-                                    </div>
-
-                                    <div class="col-span-6 sm:col-span-3">
-                                        <label for="country" class="block text-sm font-medium text-gray-700">Country</label>
-                                        <select id="country" name="country" autocomplete="country-name"
-                                            class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
-                                            <option>United States</option>
-                                            <option>Canada</option>
-                                            <option>Mexico</option>
-                                        </select>
-                                    </div>
-
-                                    <div class="col-span-6">
-                                        <label for="street-address" class="block text-sm font-medium text-gray-700">Street
-                                            address</label>
-                                        <input type="text" name="street-address" id="street-address"
-                                            autocomplete="street-address"
-                                            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
-                                    </div>
-
+                                    <!-- Primer INput -->
                                     <div class="col-span-6 sm:col-span-6 lg:col-span-2">
                                         <label for="city" class="block text-sm font-medium text-gray-700">City</label>
                                         <input type="text" name="city" id="city" autocomplete="address-level2"
                                             class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                     </div>
 
+                                    <!-- Segundo Input -->
                                     <div class="col-span-6 sm:col-span-3 lg:col-span-2">
                                         <label for="region" class="block text-sm font-medium text-gray-700">State /
                                             Province</label>
@@ -65,10 +43,12 @@
                                             class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                     </div>
 
+                                    <!-- Tercer Input -->
                                     <div class="col-span-6 sm:col-span-3 lg:col-span-2">
                                         <label for="postal-code" class="block text-sm font-medium text-gray-700">ZIP /
                                             Postal code</label>
-                                        <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code"
+                                        <input type="text" name="postal-code" id="postal-code"
+                                            autocomplete="postal-code"
                                             class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                     </div>
                                 </div>
